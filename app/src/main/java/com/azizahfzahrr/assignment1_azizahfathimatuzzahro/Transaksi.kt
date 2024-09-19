5fb68bab0f7d386978665b60a01a99e109b79475
azizahfzahr <afathimatuzzahro@gmail.com>
2024-09-19 21:43:04 +0700

    Fixing code

---
+++ app/src/main/java/com/azizahfzahrr/assignment1_azizahfathimatuzzahro/Transaksi.kt
@@ -0,0 +1,62 @@
+package com.azizahfzahrr.assignment1_azizahfathimatuzzahro
+
+import com.azizahfzahrr.assignment1_azizahfathimatuzzahro.TransaksiModel.listPemasukan
+import com.azizahfzahrr.assignment1_azizahfathimatuzzahro.TransaksiModel.pemasukan
+import com.azizahfzahrr.assignment1_azizahfathimatuzzahro.TransaksiModel.saldo
+import com.azizahfzahrr.assignment1_azizahfathimatuzzahro.TransaksiModel.totalPemasukan
+import com.azizahfzahrr.assignment1_azizahfathimatuzzahro.TransaksiModel.listPengeluaran
+import com.azizahfzahrr.assignment1_azizahfathimatuzzahro.TransaksiModel.pengeluaran
+import com.azizahfzahrr.assignment1_azizahfathimatuzzahro.TransaksiModel.totalPengeluaran
+
+class Transaksi {
+
+    fun pemasukanPengeluaran() {
+        println("1. Pemasukan")
+        println("2. Pengeluaran")
+        println("Pilih menu yang diinginkan: ")
+        val menu = readln().toIntOrNull()
+        when (menu) {
+            1 -> {
+                println("Masukkan nominal pemasukan: ")
+                pemasukan = readln().toInt()
+                saldo += pemasukan
+                listPemasukan.add(pemasukan)
+                totalPemasukan = listPemasukan.sum()
+                println("Total pemasukkan anda adalah $totalPemasukan")
+            }
+
+            2 -> {
+                println("Masukkan nominal pengeluaran: ")
+                pengeluaran = readln().toInt()
+                saldo -= pengeluaran
+                listPengeluaran.add(pengeluaran)
+                totalPengeluaran = listPengeluaran.sum()
+            } else -> println("Menu yang anda inginkan tidak valid")
+        }
+        println("Saldo anda adalah $saldo")
+    }
+    fun getSaldo () : Int{
+        return saldo
+    }
+    fun riwayatTransaksi(){
+        if (listPemasukan.isEmpty() || listPengeluaran.isEmpty()){
+            println("Riwayat transaksi tidak ditemukan")
+        } else {
+            println("Riwayat Pemasukan")
+            listPemasukan.forEach {
+                if (it > 0){
+                    println("$it")
+                }
+            }
+            println("Riwayat Pengeluaran")
+            listPengeluaran.forEach {
+                if (it > 0){
+                    println("$it")
+                }
+            }
+        }
+    }
+    fun analisisTotal(){
+        println("Jumlah pemasukan sebanyak $totalPemasukan \nJumlah pengeluaran sebanyak $totalPengeluaran \nTotal Saldo anda adalah ${getSaldo()}")
+    }
+}
---
+++ app/src/main/java/com/azizahfzahrr/assignment1_azizahfathimatuzzahro/TransaksiModel.kt
@@ -0,0 +1,11 @@
+package com.azizahfzahrr.assignment1_azizahfathimatuzzahro
+
+object TransaksiModel {
+    var pemasukan: Int = 0
+    var pengeluaran: Int =  0
+    var saldo: Int = 0
+    val listPemasukan = mutableListOf(pemasukan)
+    val listPengeluaran = mutableListOf(pengeluaran)
+    var totalPemasukan: Int =  0
+    var totalPengeluaran: Int = 0
+}
